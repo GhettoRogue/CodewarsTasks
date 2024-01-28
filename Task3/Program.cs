c0195b3afc84d391d6428dbef2828c4c7938534f
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-29 03:41:40 +0400

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿var haystack = new object[] { "hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk" };
+
+Console.Clear();
+foreach (var item in FindNeedle(haystack))
+{
+    Console.Write(item);
+}
+
+return;
+
+static string FindNeedle(object[] list)
+{
+    const string message = "Found the needle at position: ";
+    const string needle = "needle";
+
+    return message + Array.IndexOf(list, needle);
+}
