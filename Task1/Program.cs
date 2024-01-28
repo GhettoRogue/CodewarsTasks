14955661ca9274ee915828c278740fc911855e2e
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-28 22:28:46 +0400

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿var listOfNames = new List<string> { "Ryan", "Kieran", "Mark" };
+
+foreach (var friends in FriendOrFoe(listOfNames))
+{
+    Console.WriteLine(friends);
+}
+
+return;
+
+IEnumerable<string> FriendOrFoe(IEnumerable<string> names)
+{
+    return names.Where(n => n.Length == 4).ToList();
+}
