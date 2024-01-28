63595e16734623dafd7d65a4c0e5f00e3a73dac8
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-28 22:29:09 +0400

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿const string sentence = "Turns out random test cases are easier than writing out basic ones";
+
+var shortestWord = FindShort(sentence);
+Console.WriteLine(sentence);
+Console.WriteLine("The length of the shortest word is: " + shortestWord);
+
+return;
+
+static int FindShort(string sentence)
+{
+    var words = sentence.Split().Min(s => s.Length);
+
+    return words;
+}
