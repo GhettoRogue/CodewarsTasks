934714f67024df2a118d04a1a6c01a2cbf4b7328
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-02-06 19:23:51 +0400

    add main logic

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+const string pattern = "[aeiouAEIOU]";
+const string replacement = "!";
+
+Console.WriteLine(Replace("Hi!"));
+Console.WriteLine(Replace("!Hi! Hi!"));
+Console.WriteLine(Replace("ABCDE"));
+
+return;
+
+static string Replace(string str)
+{
+    return Regex.Replace(str, pattern, replacement);
+}
