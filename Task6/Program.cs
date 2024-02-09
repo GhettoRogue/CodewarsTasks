829c438cd2f7ae7c0ddfbb8b5de00f3f8b846705
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-02-09 17:59:24 +0400

    add main logic

---
+++ Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿const string sentence = "The count of occurrences: ";
+
+Console.WriteLine($"{sentence}{StrCount("Hello", 'o')}");
+Console.WriteLine($"{sentence}{StrCount("Hello", 'l')}");
+Console.WriteLine($"{sentence}{StrCount("", 'z')}");
+
+return;
+
+int StrCount(string str, char letter)
+{
+    return str.Count(l => l == letter);
+}
+
+/*int StrCountUsingForLoop(string str, char letter)
+{
+    var count = 0;
+
+    for (var i = 0; i < str.Length; i++)
+    {
+        if (str[i] == letter)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}*/
