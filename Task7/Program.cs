b95edcca97cb57a30e014f3ce04a3a0991439118
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-02-15 23:00:28 +0400

    add main logic

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var nums = new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-Console.WriteLine("Hello, World!");
+PrintEvenNumbers(nums);
+
+int[] GetEvenNumbers(int[] numbers)
+{
+    return numbers.Where(n => n % 2 == 0).ToArray();
+}
+
+void PrintEvenNumbers(int[] numbers)
+{
+    foreach (var even in GetEvenNumbers(numbers))
+    {
+        Console.WriteLine(even);
+    }
+}
