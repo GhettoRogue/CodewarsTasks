563de0ef1ad6d700a356f323f3a017c510b0e5a2
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-02-18 18:13:39 +0400

    add main logic

---
+++ Task8/Program.cs
@@ -0,0 +1,36 @@
+﻿var words = new [] {"bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"};
+
+OutputToConsoleUsingSort(words);
+
+return;
+
+string TwoSort(string[] s)
+{
+    var sorted = s.OrderBy(str => str).ToArray();
+
+    var first = sorted.First();
+  
+    var firstChars = first.ToCharArray();
+
+    return string.Join("***", firstChars);
+}
+
+void OutputToConsoleUsingSort(string[] s)
+{
+    Console.Clear();
+    foreach (var item in TwoSort(s))
+    {
+        Console.Write(item);
+    }
+}
+
+/*string TwoSortAsSeparator(string[] s, string separator)
+{
+    var sorted = s.OrderBy(str => str).ToArray();
+
+    var first = sorted.First();
+  
+    var firstChars = first.ToCharArray();
+
+    return string.Join(separator, firstChars);
+}*/
