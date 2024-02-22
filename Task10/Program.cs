30b4a3935a306bb080a75f2ae7f2ba30509503dd
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-02-22 15:25:52 +0400

    add main logic

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine(GetDrinkByProfession("programmer"));
+Console.WriteLine(GetDrinkByProfession("politician"));
+Console.WriteLine(GetDrinkByProfession("bike gang member"));
+Console.WriteLine(GetDrinkByProfession("other"));
+
+return;
+
+string GetDrinkByProfession(string p)
+{
+    return p.ToLower() switch
+    {
+        "jabroni" => "Patron Tequila",
+        "school counselor" => "Anything with Alcohol",
+        "programmer" => "Hipster Craft Beer",
+        "bike gang member" => "Moonshine",
+        "politician" => "Your tax dollars",
+        "rapper" => "Cristal",
+        _ => "Beer"
+    };
+}
