8828c73fdb19b0a744133b0130802c7685415de8
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-03-05 12:17:22 +0400

    add main logic

---
+++ Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+Console.WriteLine(ToAlternatingCase("hello world"));
+Console.WriteLine(ToAlternatingCase("1a2b3c4d5e"));
+Console.WriteLine(ToAlternatingCase("HeLLo WoRLD"));
+Console.WriteLine(ToAlternatingCase("String.ToAlternatingCase"));
+
+return;
+
+static string ToAlternatingCase(string s)
+{
+    var result = new StringBuilder();
+    foreach (var item in s)
+    {
+        if (char.IsLetter(item))
+        {
+            result.Append(char.IsUpper(item)
+                ? char.ToLower(item)
+                : char.ToUpper(item));
+        }
+        else
+        {
+            result.Append(item);
+        }
+    }
+
+    return result.ToString();
+}
