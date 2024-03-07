4968da9cbf22c45e90999d8ad784896c740c8695
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-03-07 17:36:07 +0400

    add main logic

---
+++ Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine($"Number of digit is: {Digits(03456)}");
+Console.WriteLine($"Number of digit is: {Digits(34567)}");
+Console.WriteLine($"Number of digit is: {Digits(345678)}");
+
+return;
+
+static int Digits(ulong n)
+{
+    var myStr = Convert.ToString(n);
+    return  myStr.Length;
+    
+    /*var count = 0;
+    while (n != 0)
+    {
+        n /= 10;
+        count++;
+    }
+    return count;*/
+}
