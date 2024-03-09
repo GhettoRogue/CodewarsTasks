311d29e0b47020fc1afc071e7a50ee4b73972188
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-03-10 00:01:44 +0400

    add main logic

---
+++ Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+Console.WriteLine(AbbrevName(""));
+
+Console.WriteLine(AbbrevName("john doe"));
+Console.WriteLine(AbbrevName("John Doe"));
+Console.WriteLine(AbbrevName("Evan Cole"));
+Console.WriteLine(AbbrevName("P Favuzzi"));
+Console.WriteLine(AbbrevName("David Mendieta"));
+
+return;
+
+string AbbrevName(string name)
+{
+    if (name.Length == 0)
+        return string.Empty;
+    for (var i = 0; i < name.Length; i++)
+    {
+        if (name[i] == ' ')
+        {
+            return name.ToUpper()[0] + "." + name.ToUpper()[i + 1];
+        }
+    }
+
+    return name;
+}
